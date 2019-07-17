c483c9df7019bae75268c22f19ad1e51e850a573
Surly Dev <surly@surlydev.net>
2019-07-17 09:29:43 +0100

    Comments added for more tests I want to write

--- test.py
+++ test.py
@@ -5,63 +5,98 @@ from game import Game
 
 class TestConway(unittest.TestCase):
     # Create a Game class that takes a numpy array for the seed
-    def test_game_init(self):
-        game = Game(12, 12)
-        self.assertEqual(game.board_size, (12, 12))
+#    def test_game_init(self):
+#        game = Game(12, 12)
+#        self.assertEqual(game.board_size, (12, 12))
 
     # Add a beacon with a single cell active
-    def test_game_init_beacon(self):
-        beacon = np.zeros((6, 6))
-        beacon[1, 1] = 1
-        game = Game(6, 6, beacon)
-        self.assertTrue(np.array_equal(game.beacon, beacon))
+#    def test_game_init_beacon(self):
+#        beacon = np.zeros((6, 6))
+#        beacon[1, 1] = 1
+#        game = Game(6, 6, beacon)
+#        self.assertTrue(np.array_equal(game.beacon, beacon))
 
     # Test for defaults
-    def test_game_init_defaults(self):
-        game = Game()
-        self.assertEqual(game.board_size, (6, 6))
-        self.assertTrue(np.array_equal(game.beacon, np.zeros((6, 6))))
+#    def test_game_init_defaults(self):
+#        game = Game()
+#        self.assertEqual(game.board_size, (6, 6))
+#        self.assertTrue(np.array_equal(game.beacon, np.zeros((6, 6))))
 
     # Test for default board_size on set beacon
-    def test_game_init_board_default_beacon(self):
-        game = Game(12, 12)
-        self.assertTrue(np.array_equal(game.beacon, np.zeros((12, 12))))
+#    def test_game_init_board_default_beacon(self):
+#        game = Game(12, 12)
+#        self.assertTrue(np.array_equal(game.beacon, np.zeros((12, 12))))
 
     # Test that check cell returns a valid value
-    def test_game_check_cell_with_one_neighbor(self):
-        beacon = [[0, 1, 0]
-                , [0, 1, 0]
-                , [0, 0, 0]]
-        game = Game(3, 3, beacon)
-        self.assertEqual(game._numNeighbors(1, 1), 1)
+#    def test_game_check_cell_with_one_neighbor(self):
+#        beacon = [[0, 1, 0]
+#                , [0, 1, 0]
+#                , [0, 0, 0]]
+#        game = Game(3, 3, beacon)
+#        self.assertEqual(game._numNeighbors(1, 1), 1)
 
-    def test_game_check_cell_with_two_neighbors(self):
-        beacon = [[0, 1, 0]
-                , [0, 1, 0]
-                , [0, 0, 0]]
-        game = Game(3, 3, beacon)
-        self.assertEqual(game._numNeighbors(1, 2), 2)
+#    def test_game_check_cell_with_two_neighbors(self):
+#        beacon = [[0, 1, 0]
+#                , [0, 1, 0]
+#                , [0, 0, 0]]
+#        game = Game(3, 3, beacon)
+#        self.assertEqual(game._numNeighbors(1, 2), 2)
 
-    def test_game_check_top_row_cell_with_one_neighbour(self):
-        beacon = [[0, 0, 1]
-                , [0, 0, 0]
-                , [0, 0, 0]]
-        game = Game(3, 3, beacon)
-        self.assertEqual(game._numNeighbors(1, 2), 1)
+#    def test_game_check_top_row_cell_with_one_neighbour(self):
+#        beacon = [[0, 0, 1]
+#                , [0, 0, 0]
+#                , [0, 0, 0]]
+#        game = Game(3, 3, beacon)
+#        self.assertEqual(game._numNeighbors(1, 2), 1)
+#
+#    def test_game_check_right_column_top_row_cell_with_one_neighbour(self):
+#        beacon = [[0, 0, 1]
+#                , [1, 1, 0]
+#                , [0, 0, 0]]
+#        game = Game(3, 3, beacon)
+#        self.assertEqual(game._numNeighbors(2, 0), 1)
 
-    def test_game_check_right_column_top_row_cell_with_one_neighbour(self):
-        beacon = [[0, 0, 1]
+#    def test_game_check_right_column_bottom_row_cell_with_two_neighbours(self):
+#        beacon = [[1, 1, 1]
+#                , [1, 1, 1]
+#                , [0, 0, 1]]
+#        game = Game(3, 3, beacon)
+#        self.assertEqual(game._numNeighbors(2, 2), 2) 
+
+#    def test_game_check_top_left_with_no_neighbours(self):
+#        beacon = [[0, 0, 0]
+#                , [0, 0, 0]
+#                , [0, 0, 0]]
+#        game = Game(3, 3, beacon)
+#        self.assertEqual(game._numNeighbors(0, 0), 0) 
+
+    def test_game_check_top_left_with_three_neighbours(self):
+        print("TL 3 N")
+        beacon = [[1, 1, 0]
                 , [1, 1, 0]
                 , [0, 0, 0]]
         game = Game(3, 3, beacon)
-        self.assertEqual(game._numNeighbors(2, 0), 1)
+        self.assertEqual(game._numNeighbors(0, 0), 3) 
 
-    def test_game_check_right_column_bottom_row_cell_with_two_neighbours(self):
-        beacon = [[1, 1, 1]
-                , [1, 1, 1]
-                , [0, 0, 1]]
-        game = Game(3, 3, beacon)
-        self.assertEqual(game._numNeighbors(2, 2), 2) 
+
+# top right with three neighbours
+
+# bottom right with three neighbours
+
+# bottom left with three neighbours
+
+# inverse 0's and 1's top left ?
+
+
+
+
+#    def test_game_check_centre_cell_with_eight_neighbours(self):
+#        beacon = [[1, 1, 1]
+#                , [1, 1, 1]
+#                , [1, 1, 1]]
+#        game = Game(3, 3, beacon)
+#        self.assertEqual(game._numNeighbors(1, 1), 8)
+
 
     #def test_single_column_array(self):
     #    beacon = [[0], [0], [1]]
