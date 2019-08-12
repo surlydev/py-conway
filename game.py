3d221629278528ea0aff0637fc307d0f098ee428
Surly Dev <surly@surlydev.net>
2019-08-12 09:58:10 +0100

    more debugging code, and off-by-one fix on board width and height comparison

--- game.py
+++ game.py
@@ -12,11 +12,13 @@ class Game:
             self.beacon = beacon
         self.state = self.beacon
 
+
     def _numNeighbors(self, x, y, debugMode=0):
         if debugMode == 1:
             print '---Game.py _numNeighbours ---'
             print 'Debug Mode: ON'
-            print 'x=' + str(x) + ' y=' + str(y)
+            print ' x=' + str(x) + ' y=' + str(y)
+            print self.state
         startX = 0
         startY = 0
         endX = 0
@@ -24,49 +26,72 @@ class Game:
         neighbors = 0
         boardWidth = self.board_size[0]
         boardHeight = self.board_size[1]
+        if debugMode ==1:
+            print 'Board width = ' + str(boardWidth) + ' height = ' + str(boardHeight)
 
         if (x > 0):
             startX = -1
         if (y > 0):
             startY = -1
 
-        if x < boardWidth:
+        #Need to subtract 1 from the board width and height for this comparison, due to the grid being 0-based
+        if x < boardWidth - 1:
             endX = 1
-        if y < boardHeight:
+        if y < boardHeight - 1:
             endY = 1
 
         if debugMode == 1:
-            print ' x: ' + str(x) + ' y: ' + str(y)
-            print ' startX: ' + str(startX) + ' endX: ' + str(endX)
-            print ' startY: ' + str(startY) + ' endY: ' + str(endY)
+            print ' startX: ' + str(startX) + ' to endX: ' + str(endX)
+            print ' startY: ' + str(startY) + ' to endY: ' + str(endY)
 
-        if debugMode == 1:
-            #here because of my lack of understanding of Python
-            print 'checking X range'
-            for b in range(startX, endX + 1): #needs a +1 on the endX range to include it
-                print ' value = ' + str(b)
+#        if debugMode == 1:
+#            #here because of my lack of understanding of Python
+#            print 'checking X range'
+#            for b in range(startX, endX + 1): #needs a +1 on the endX range to include it
+#                print ' value = ' + str(b)
                 
-        if debugMode == 1:
-            #here because of my lack of understanding of Python
-            print 'checking Y range'
-            for b in range(startY, endY + 1): #needs a +1 on the endY range to include it
-                print ' value = ' + str(b)
+#        if debugMode == 1:
+#            #here because of my lack of understanding of Python
+#            print 'checking Y range'
+#            for b in range(startY, endY + 1): #needs a +1 on the endY range to include it
+#                print ' value = ' + str(b)
 
         if debugMode == 1:
             print 'Starting to check the ranges for all neighbours'
 
-        for checkY in range(startY, endY + 1):
-            for checkX in range(startX, endX + 1):
-                if debugMode ==1:
-                    print ' checking ' + str(x + checkX) + ', ' + str(y + checkY)
-                if not (checkX == 0 and checkY == 0):
-                    if debugMode == 1:
-                        print ' state :' + str(self.state[x + checkX][y + checkY])
-                    if (self.state[x + checkX][y + checkY] == 1): 
-                        neighbors += 1
-                        if debugMode == 1:
-                            print ' found a neighbour at ' + str(x + checkX) + ', ' + str(y + checkY) + '. That''s ' + str(neighbors) + ' found.'
 
+        for checkY in range(y + startY, y + endY + 1):
+            if debugMode ==1:
+                print ' --> Row:' + str(checkY)
+            for checkX in range(x + startX, x + endX + 1):
+                if debugMode ==1:
+                    print ' checking ' + str(checkX) + ', ' + str(checkY)
+                    print 'state: ' + str(self.state[checkX][checkY])
+
+                   #print 'checking truthy test '
+                    #print (checkX == x and checkY == y)
+
+                if (checkX == x and checkY == y):
+                    if debugMode ==1:
+                        print ' skipping the x, y co-ordinate start point'
+
+                if not (checkX == x and checkY == y):
+                    #if debugMode == 1:
+                        #print ' checking state'
+                        #print ' state :' + str(self.state[checkX][checkY])
+                    if (self.state[checkX][checkY] == 1): 
+                        neighbors += 1
+
+                        if debugMode == 1:
+                            print ' found a neighbour at ' + str(checkX) + ', ' + str(checkY) + '. That''s ' + str(neighbors) + ' found.'
+                    print 'state: ' + str(self.state[checkX][checkY])
+
+
+            if debugMode == 1:
+                print 'finished checking the row'
+            
+        if debugMode == 1:
+            print 'finished checking all the rows'
         return neighbors
 
     def step(self):
